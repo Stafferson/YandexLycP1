ef8305b7eda6eae9b037ebd83129185d8f190413
Stafferson <71585948+Stafferson@users.noreply.github.com>
2021-11-15 14:29:45 +0600

    fuck this unoptimized shit (x5)

--- Find_movies.py
+++ Find_movies.py
@@ -6,6 +6,9 @@ from pprint import pprint
 
 import requests
 import sys
+
+from PyQt5 import QtCore
+
 import Film_info_screen
 
 from PyQt5.QtWidgets import *
@@ -14,13 +17,7 @@ import Main_Screen
 import global_vars
 from Main_Screen import *
 
-counter = 10
-res = {}
-
-
-class Liked_movies(QWidget):
-
-    any_favorite = False
+class Find_movies(QWidget):
 
     def __init__(self):
         super().__init__()
@@ -29,23 +26,21 @@ class Liked_movies(QWidget):
     def initUI(self):
         global imdb_dict
         global res
-        super(Liked_movies, self).__init__()
+        super().__init__()
         self.setGeometry(0, 0, 600, 1000)
         self.setWindowTitle("Films App")
         self.w = None
 
         self.label1 = QListWidget(self)
-        self.label1.setGeometry(20, 20, 560, 800)
-        con = sqlite3.connect("yandex_project_1.sqlite")
-        cur = con.cursor()
-        query = "SELECT film_id from film_favorite"
-        res = cur.execute(query).fetchall()
+        self.label1.setGeometry(20, 220, 560, 600)
+        #con = sqlite3.connect("yandex_project_1.sqlite")
+        #cur = con.cursor()
+        #query = "SELECT film_id from film_favorite"
+        #res = cur.execute(query).fetchall()
         #print("GAY11111111111111111111!")
         #pprint(res)
         #print(len(res))
 
-        thread1 = threading.Thread(target=self.thread_function)
-        thread1.start()
         """
         if (len(res) == 0):
             for i in range(10):
@@ -70,11 +65,37 @@ class Liked_movies(QWidget):
         self.label2.setText("Go back")
         self.label2.clicked.connect(self.back)
 
+        self.label3 = QLineEdit(self)
+        self.label3.setGeometry(20, 20, 560, 100)
+        self.label3.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.label4 = QPushButton(self)
+        self.label4.setGeometry(250, 140, 100, 60)
+        self.label4.setText("Search")
+        self.label4.clicked.connect(self.find_movie)
+
     def click(self, item):
         Film_info_screen.film_full_title = item.text()
         self.w = Film_info_screen.Film_screen()
         self.w.show()
         self.hide()
+
+    def find_movie(self):
+        api_link = "https://imdb-api.com/en/API/Title/" + global_vars.var_API + "/" + self.label3.text()
+        # print(api_link)
+        # print("!!!!!!!!!")
+        # print("12")
+        # print(res[i][0])
+        # api_link = "https://imdb-api.com/en/API/Title/k_8fn0w7v6/tt0110413"
+        # print(api_link)
+        # self.get_info(search_type, film_title, api_link)
+        response = requests.get(api_link)
+        film_dict = json.loads(response.text)
+        pprint(film_dict)
+        if (film_dict["fullTitle"] == None):
+            #print("EYSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS")
+            self.label4.setText("Try again")
+        #else:
 
     def back(self):
         if self.w is None:
@@ -89,31 +110,6 @@ class Liked_movies(QWidget):
         #self.w.show()
         ##self.hide()
         #self.close()
-
-    def thread_function(self):
-        global res
-
-        if (len(res) == 0):
-            for i in range(10):
-                self.label1.addItem("Add some movies first")
-        else:
-            self.any_favorite = True
-            #print(len(res))
-            for i in range(len(res)):
-                api_link = "https://imdb-api.com/en/API/Title/" + global_vars.var_API + "/" + res[i][0]
-                #print("12")
-                #print(res[i][0])
-                #api_link = "https://imdb-api.com/en/API/Title/k_8fn0w7v6/tt0110413"
-                #print(api_link)
-                #self.get_info(search_type, film_title, api_link)
-                response = requests.get(api_link)
-                fav_film_dict = json.loads(response.text)
-                #pprint(fav_film_dict)
-                self.label1.addItem(fav_film_dict["fullTitle"])
-                #print(fav_film_dict["fullTitle"])
-                self.label1.itemClicked.connect(self.click)
-                #print("12121")
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- global_vars.py
+++ global_vars.py
@@ -1,1 +1,2 @@
-var_API = "k_3o5mqrh2"
+#var_API = "k_3o5mqrh2"
+var_API = "k_8fn0w7v6"
